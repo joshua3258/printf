0869e11cbb7b3d318e45499a73b0aede3c889cc8
joshua3258 <odebodejoshua18@gmail.com>
2023-11-14 15:00:30 -0800

    adjust pointer

--- printf_pointer.c
+++ printf_pointer.c
@@ -13,13 +13,11 @@ int printf_pointer(va_list val)
 	int b;
 	int i;
 
-	p = va_arg(val, void*);
+	p = va_arg(val, void *);
 	if (p == NULL)
 	{
 		for (i = 0; s[i] != '\0'; i++)
-		{
 			_putchar(s[i]);
-		}
 		return (i);
 	}
 
